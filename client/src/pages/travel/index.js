c1e67b48e687abbba2853a9d535592e29f99bf67
nathansabin <sabinnat001@gmail.com>
2024-01-29 17:30:52 -0800

    added input form for geocoding

--- client/src/pages/travel/index.js
+++ client/src/pages/travel/index.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import MapContainer from '../../components/map';
 
 const Travel = () => {
+    const [location, setLocation] = useState("");
+    const [cordinates, setCordinates] = useState("");
+
+    const locationChange = (event) => {
+        const {value} = event.target
+        setLocation(value);
+    }
+
+    const updateLocation = (event) => {
+        event.preventDefault()
+        
+    }
+
     return (
         <div>
+           <section>
+                <form onSubmit={updateLocation}>
+                    <label>Enter a location.</label>
+                    <input onChange={locationChange} id="city" name="city" type="text" />
+                    <button type="submit">Choose location!</button>
+                </form>
+           </section>
            <MapContainer />
         </div>
     );
